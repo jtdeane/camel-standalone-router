fb0193b0d9e126c25119f53a2e7a514bba0387b3
Jeremy Deane <jeremy.deane@gmail.com>
2015-07-25 17:36:19 -0400

    Moved some routes to another example

--- src/main/java/ws/cogito/magic/MagicRouteBuilder.java
+++ src/main/java/ws/cogito/magic/MagicRouteBuilder.java
@@ -1,7 +1,10 @@
 package ws.cogito.magic;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.xml.XPathBuilder;
+import org.apache.camel.model.dataformat.XmlJsonDataFormat;
 
 /**
  * Magic Route Builder
@@ -23,10 +26,10 @@ public class MagicRouteBuilder extends RouteBuilder {
     	/**
     	 * Splitter - xpath expression
     	 */
-    	from("activemq:emagic.orders").
-    		split(splitXPath).
-    		parallelProcessing().
-    	to("activemq:emagic.order");
+		from("activemq:emagic.orders").
+			split(splitXPath).parallelProcessing().
+			wireTap("direct:ministry").
+		to("activemq:emagic.order");
     	
     	/**
     	 * Content Based Routing - simple expression
@@ -37,5 +40,40 @@ public class MagicRouteBuilder extends RouteBuilder {
     			to("activemq:priority.order").
     		otherwise().
     			to("activemq:magic.order");
+    	/**
+    	 * Content Based Routing - Mediation, simple expression
+	 
+	XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
+	xmlJsonFormat.setForceTopLevelObject(true);
+		
+    	from("activemq:emagic.order").
+    	choice().
+    		when().simple("${in.body} contains 'Houdini'").
+				process(new Processor() {
+	        		public void process(Exchange exchange) {	
+	        			exchange.getIn().setHeader("VIP", "true");
+	        		}
+	        	}).
+    			to("activemq:priority.order").
+    		otherwise().
+    			marshal(xmlJsonFormat).
+    			transform(body().regexReplaceAll("@", "")).
+    			to("activemq:magic.order");*/
+    	
+    	
+    	/**
+    	 * Content Based Routing - Wire-Tap to ActiveMQ Topic 
+    	 
+    	from("activemq:emagic.order").
+    		wireTap("direct:ministry").
+    	to("activemq:magic.order");
+    	
+    	from("direct:ministry").
+			choice().
+				when().simple("${in.body} contains 'Elder Wand'").
+					log("ILLEGAL MAGIC ALERT").
+					to("activemq:topic:magic.alerts").		
+				otherwise().
+					log("...off into the ether");*/	
     }
 }
