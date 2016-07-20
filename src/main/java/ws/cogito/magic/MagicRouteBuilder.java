b21666faae1f3acbee9eaf7cd4b34622e7c0a421
Jeremy Deane <jeremy.deane@gmail.com>
2016-07-20 09:40:54 -0600

    reset to original state for workshop

--- src/main/java/ws/cogito/magic/MagicRouteBuilder.java
+++ src/main/java/ws/cogito/magic/MagicRouteBuilder.java
@@ -16,8 +16,7 @@ public class MagicRouteBuilder extends RouteBuilder {
     	/*
     	 * Route errors to DLQ after one retry and one second delay
     	 */
-    	errorHandler(deadLetterChannel("activemq:emagic.dead").
-    			maximumRedeliveries(1).redeliveryDelay(1000));
+    	errorHandler(deadLetterChannel("activemq:queue:emagic.dead"));
     	
 		XPathBuilder splitXPath = new XPathBuilder (splitXpath);
 		
@@ -31,15 +30,16 @@ public class MagicRouteBuilder extends RouteBuilder {
     	
     	/*
     	 * Content Based Routing - simple expression
-    	 
+    	 */
     	from("activemq:emagic.order").
     	choice().
     		when().simple("${in.body} contains 'Houdini'").
     			to("activemq:priority.order").
     		otherwise().
-    			to("activemq:magic.order");*/
+    			to("activemq:magic.order");
+    }
     	
-		/*
+	/*
     	 * Content Based Routing - Mediation, simple expression
     	 * http://camel.apache.org/maven/camel-2.15.0/camel-core/apidocs/org/apache/camel/Processor.html
     	 
@@ -54,12 +54,13 @@ public class MagicRouteBuilder extends RouteBuilder {
     		otherwise().
     			marshal(xmlJsonFormat).
     			transform(body().regexReplaceAll("@", "")).
-    			to("activemq:magic.order");  */  	
-    	
+    			to("activemq:magic.order");    	
+    }*/
+		
     	/*
     	 * Content Based Routing - Wire-Tap to ActiveMQ Topic
     	 * Requires updating Splitter Route and Uncomment wireTap 
-    	 */
+    	 
     	from("activemq:emagic.order").
     		wireTap("direct:ministry").
     	to("activemq:magic.order");
@@ -71,5 +72,5 @@ public class MagicRouteBuilder extends RouteBuilder {
 					to("activemq:topic:magic.alerts").		
 				otherwise().
 					log("...off into the ether");
-    }
+    }*/
 }
